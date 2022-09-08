c0863aebc2239c5418736d4301d757016baffcb7
Daniel Wahbi <xed255@gmail.com>
2022-09-08 20:22:06 +0300

    add pattern Strategy

--- Program.cs
+++ Program.cs
@@ -1,211 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Laba1
 {
-    public class Сalculator
+    public interface IOperationsBehavior
     {
-        public float Value { get; private set; }
+        T PerformValue<T>(ref Stack<T> stack, char sign);
+        T ConvertChar<T>(char symbol);
+    }
+
+    public interface IOperationPriority
+    {
+        (char, int) GetPriority(char c);
+    }
+
+    public abstract class Operations
+    {
+        private IOperationsBehavior _operationsBehavior;
+        private IOperationPriority _operationsPriority;
+
+        public void SetOperationBehavior(IOperationsBehavior operationsBehavior) =>
+            _operationsBehavior = operationsBehavior;
+
+        public void SetOperationPriority(IOperationsBehavior operationPriority) =>
+            _operationsBehavior = operationPriority;
+
+        public T PerformOperationBehavior<T>(ref Stack<T> stack, char sign) =>
+            _operationsBehavior.PerformValue(ref stack, sign);
+        public T PerformConvertValue<T>(char symbol) =>
+            _operationsBehavior.ConvertChar<T>(symbol);
+
+        protected (char, int) PerformOperationPriority(char c) => _operationsPriority.GetPriority(c);
+    }
+
+    public class Сalculator<T> : Operations
+    {
+        public T ValueExpression { get; private set; }
 
         public Сalculator(string expression)
         {
-            CalculatingExpression(GetOutputReversePolishNotation(expression));
+            ValueExpression = CalculatingExpression(GetOutputReversePolishNotation(expression));
         }
 
-        private int GetPriority(char c)
+        private string GetOutputReversePolishNotation(string expression)
         {
-            switch (c)
+            void DeleteWithReplacement(ref string s, ref Stack<char> stack)
             {
-                case '(':
-                case ')':
-                    return 0;
-                case '*':
-                case '/':
-                    return 1;
-                case '+':
-                case '-':
-                    return 2;
-                default:
-                    return 3;
-            }
-        }
-
-        private float Perform(ref Stack<float> stack, char sign)
-        {
-            var value = 0f;
-
-            float a, b = 0;
-            switch (sign)
-            {
-                case '*':
-                    a = stack.Peek();
-                    stack.Pop();
-                    b = stack.Peek();
-                    stack.Pop();
-
-                    value = b * a;
-
-                    break;
-                case '/':
-                    a = stack.Peek();
-                    stack.Pop();
-                    b = stack.Peek();
-                    stack.Pop();
-
-                    value = b / a;
-
-                    break;
-                case '+':
-                    a = stack.Peek();
-                    stack.Pop();
-                    b = stack.Peek();
-                    stack.Pop();
-
-                    value = b + a;
-
-                    break;
-                case '-':
-                    a = stack.Peek();
-                    stack.Pop();
-                    b = stack.Peek();
-                    stack.Pop();
-
-                    value = b - a;
-
-                    break;
-            }
-
-            return value;
-        }
-
-        private List<char> GetOutputReversePolishNotation(string expression)
-        {
-            void OrderSigns(ref Stack<char> s)
-            {
-                var top = '\0';
-                var bottom = '\0';
-                if (s.Count >= 2)
-                {
-                    top = s.Peek();
-                    s.Pop();
-                    bottom = s.Peek();
-                    s.Push(top);
-
-                    if (GetPriority(bottom) == 1)
-                    {
-                        s.Pop();
-                        s.Pop();
-
-                        s.Push(top);
-                        s.Push(bottom);
-                    }
-                }
-            }
-
-            var output = new List<char>();
-            var stack = new Stack<char>();
-
-            for (var i = 0; i < expression.Length; i++)
-            {
-                var symbol = expression[i];
-
-                if (char.IsDigit(symbol))
-                {
-                    output.Add(symbol);
-
-                    if (stack.Count != 0 && GetPriority(stack.Peek()) == 1)
-                    {
-                        output.Add(stack.Peek());
-                        stack.Pop();
-                    }
-                }
-                else
-                {
-                    char next;
-
-                    switch (symbol)
-                    {
-                        case '(':
-                            stack.Push(symbol);
-                            break;
-                        case ')':
-                        {
-                            while (stack.Peek() != '(')
-                            {
-                                output.Add(stack.Peek());
-                                stack.Pop();
-                            }
-
-                            stack.Pop();
-
-                            if (stack.Count == 1 && GetPriority(stack.Peek()) == 1)
-                            {
-                                output.Add(stack.Peek());
-                                stack.Pop();
-                            }
-
-                            break;
-                        }
-                        case '*':
-                        case '/':
-                        {
-                            next = expression[i + 1];
-                            if (next == '(' || char.IsDigit(next))
-                                stack.Push(symbol);
-                            else
-                                output.Add(symbol);
-
-                            break;
-                        }
-                        case '+':
-                        case '-':
-                        {
-                            next = expression[i + 1];
-                            if (next == '(' || char.IsDigit(next))
-                            {
-                                if (stack.Count != 0)
-                                {
-                                    stack.Push(symbol);
-                                    OrderSigns(ref stack);
-                                }
-                                else
-                                    stack.Push(symbol);
-                            }
-                            else
-                                output.Add(symbol);
-
-                            break;
-                        }
-                    }
-                }
-            }
-
-            while (stack.Count != 0)
-            {
-                output.Add(stack.Peek());
+                s += stack.Peek();
                 stack.Pop();
             }
 
-            return output;
-        }
-
-        private void CalculatingExpression(in List<char> expression)
-        {
-            int ConvertCharToDigit(char c) => c - '0';
-
-            var resultStack = new Stack<int>();
+            var polishNotation = "";
+            var operations = new Stack<char>();
 
             foreach (var symbol in expression)
             {
                 if (char.IsDigit(symbol))
-                    resultStack.Push(ConvertCharToDigit(symbol));
+                {
+                    polishNotation += symbol;
+                }
+
                 else
                 {
-                    resultStack.Push(Perform(ref resultStack, symbol));
+                    if (symbol == '(')
+                    {
+                        operations.Push(symbol);
+                    }
+                    else if (symbol == ')')
+                    {
+                        while (operations.Peek() != '(')
+                            DeleteWithReplacement(ref polishNotation, ref operations);
+
+                        operations.Pop();
+                    }
+                    else
+                    {
+                        while (operations.Count != 0 && PerformOperationPriority(operations.Peek()).Item2 >=
+                               PerformOperationPriority(symbol).Item2)
+                            DeleteWithReplacement(ref polishNotation, ref operations);
+
+                        if (!(operations.Count != 0 && PerformOperationPriority(operations.Peek()).Item2 >=
+                                PerformOperationPriority(symbol).Item2))
+                            operations.Push(symbol);
+                    }
                 }
             }
 
-            Value = resultStack.Peek();
+            while (operations.Count != 0)
+            {
+                polishNotation += operations.Peek();
+                operations.Pop();
+            }
+
+            return polishNotation;
+        }
+
+        private T CalculatingExpression(in string expression)
+        {
+            var resultStack = new Stack<T>();
+
+            foreach (var symbol in expression)
+            {
+                if (char.IsDigit(symbol))
+                    resultStack.Push(PerformConvertValue<T>(symbol));
+                else
+                {
+                    resultStack.Push(PerformOperationBehavior(ref resultStack, symbol));
+                }
+            }
+
+            return resultStack.Peek();
         }
     }
 
@@ -215,8 +120,8 @@ namespace Laba1
     {
         public static void Main(string[] args)
         {
-            var a = new Сalculator(Console.ReadLine());
-            Console.WriteLine(a.Value);
+            //var a = new Сalculator<T>("2*(1+5)+3*(6-4)*6+3*2+7-5*(2-(5+1*6))*3+2");
+            //Console.WriteLine(a.ValueExpression);
         }
     }
 }
